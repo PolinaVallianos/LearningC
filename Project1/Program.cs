96f9a4099edb2e937f98867136dfeff93fa753e3
PolinaVallianos <115357960+PolinaVallianos@users.noreply.github.com>
2022-10-11 16:15:07 +0200

    ClassWork

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
+float a = 10000.1f;
+float b = 10000.01f;
+double c = 0.125467;
+b = (float)c;
+Console.WriteLine(c);
+byte b1 = 51;
+int i1 = 256;
+b1 = (byte)i1;
+Console.WriteLine(b1);
