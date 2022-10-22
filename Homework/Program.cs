ccb5be0050d549beae8a6ecd24b185313f045b73
PolinaVallianos <115357960+PolinaVallianos@users.noreply.github.com>
2022-10-22 16:23:26 +0200

    Homework3

---
+++ Homework/Program.cs
@@ -0,0 +1,36 @@
+﻿int intNumber = 0;
+byte byteNumber = 0;
+string input;
+int number;
+
+Console.WriteLine("Enter integer number: ");
+input = Console.ReadLine();
+number = int.Parse(input);
+
+if (number <= 255)
+{
+    byteNumber = (byte)(byteNumber + number);;
+}
+else
+{
+    intNumber += number;
+}
+
+byteNumber += 1;
+intNumber += 1;
+
+if (byteNumber <= 1)
+{
+    byteNumber = (byte)intNumber;
+}
+else if (intNumber == 1)
+{
+    intNumber = byteNumber;
+}
+
+int difference = (int)(intNumber - byteNumber);
+int hundreds = intNumber / 100;
+int decades = intNumber % 100 / 10;
+int units = intNumber % 100 % 10;
+
+Console.Write($"Int:{intNumber} \nByte:{byteNumber} \nDiff:{difference} \n{hundreds} \n{decades} \n{units}");
--- Lesson3.2/Program.cs
+++ Lesson3.2/Program.cs
@@ -27,3 +27,5 @@ Console.WriteLine(byteNumber+2 >= shortNumber);
 Console.WriteLine(byteNumber <= shortNumber);
 Console.WriteLine(byteNumber == shortNumber);
 Console.WriteLine(byteNumber != shortNumber);
+
+Console.ReadLine();
