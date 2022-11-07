3e94b27dc92fced9cbcc91dc220deec6040e5fff
PolinaVallianos <115357960+PolinaVallianos@users.noreply.github.com>
2022-11-07 21:13:49 +0100

    Homework5

---
+++ Homework5/Program.cs
@@ -0,0 +1,29 @@
+﻿int number1;
+int number2;
+
+Console.WriteLine("Enter number1: ");
+number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number2: ");
+number2 = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = number1; i <= number2; i++)
+{
+    for (int j = number1; j <= number2; j++)
+    {
+        if (i!= j && (i + j) % 3 == 0)
+        {
+            sum = i + j;
+            Console.WriteLine($"{i} + {j} = {sum}");
+        }
+    }
+}
+
+int sum1 = 0;
+
+for (int i = number1; i <= number2; i++)
+{
+    sum1 += i;
+}
+Console.WriteLine($"Sum = {sum1}");
