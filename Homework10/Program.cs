8d036c5c2e72145ba5ba5aaf19483bd78da13d35
PolinaVallianos <115357960+PolinaVallianos@users.noreply.github.com>
2022-12-20 16:23:25 +0100

    Lesson11

--- Homework10/Program.cs
+++ Homework10/Program.cs
@@ -7,9 +7,11 @@ class Program
     {
         string string1 = @"String escape sequences. Sequence, Character name, Unicode encoding. \', Single quote, 0x0027. \"", Double quote, 0x0022. \\, Backslash, 0x005C. \0, Null, 0x0000. \n, New line, 0x000A. \t, Horizontal tab, 0x0009";
         string string2 = "Currency codes. Char, Escape sequence, HTML code, Description. \u0024, \\u0024, &#36, dollar. \u00A2, \\u00A2, &#162, cent. \u00A3, \\u00A3, &#163, pound. \u20BD, \\u20BD, &#8381, ruble";
+        string string3 = "List of companies.Company,Contact,Country.Alfreds Futterkiste,Maria Anders,Germany.Centro comercial Moctezuma,Francisco Chang,Mexico.Ernst Handel,Roland Mendel,Austria.Island Trading,Helen Bennett,United Kingdom.Laughing Bacchus Winecellars,Yoshi Tannamuri,Canada.Magazzini Alimentari Riuniti,Giovanni Rovelli,Italy.";
 
         PrintTable(string1);
         PrintTable(string2);
+        PrintTable(string3);
     }
 
     static void PrintTable(string str)
---
+++ Lesson11/Program.cs
@@ -0,0 +1,147 @@
+﻿namespace Lesson11;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Отличие бууквальных строк от необработанных
+        string str1;
+        str1 = @"\files\""My files""\";
+        str1 = """\files\"My files"\""";
+
+        string name = "Pi number";
+        float pi = 3.1415f;
+
+        str1 = $@"{{{name}}} = {pi}";
+        str1 = $"""{name} = {pi}""";
+
+        //        str1 = $@";
+        //config {{
+        //    {{name}}: ""{name}"
+        //    { { value} }: ""{pi} ""
+        //}}";
+
+
+        //        str1 = $$"""
+        //        config {
+        //            {name}: "{{name}}"
+        //            {value}: "{{pi}}"
+        //        }
+        //        """;
+
+
+        //------------------------
+
+
+        str1 = "Abcdefgh";
+        string str2 = "Cities of Montenegro: Podgorica, Bar, Cetine. Podgorica is thee capital. (podgorica)";
+
+        //проверяет содержит ли строка символ или подствроку
+        Console.WriteLinee("\n--Contains");
+
+        bool ContainceA = Contains('A'); //True
+        
+        if (str2.Contains("Bar"))
+        {
+            Console.WriteLine("Bar is in Montenegro");
+        }
+
+        bool containceAnyText = str1.ToLowwer().Contain("any text");
+
+        //заменяет символ или подстроку на новые
+        Console.WriteLine("\n-----Replace");
+
+        Console.WriteLine(str2.Replace(' ', '_'));
+        Console.WriteLine(str2.Replace("Podgorica", "Cetine"));
+
+        //Проверяет начинается ли строка с символа тлт подстроки
+        Console.WriteLine("\n---StraWitch");
+
+        Console.WriteLine("str1 start with Abc = " + str1.StartWith("Abc"));
+
+        string[] splitStr = str2.Split();
+
+        foreach (var item in splitStr)
+        {
+            if (item.ToLower().Trim().StartsWith('c'))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        //проверяет заканчивается ли строка на символ или подстроку
+        Console.WriteLine("\n---EndWith");
+
+        foreach (var item in splitStr)
+        {
+            if (item.ToLower().Trim().EndsWith('.'))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        //возращают позицию подстроки или символа. Если не найдено, вернет -1
+        Console.WriteLine("\n---IndexOf & LastIndeexOf");
+
+        Console.WriteLine(str1.IndexOf('G'));
+        Console.WriteLine(str1.IndexOf('g'));
+
+        //найти слово "Bar" и удалить его
+        Console.WriteLine(str2.Remove(str2.IndexOf("Bar"), 5));
+
+        Console.WriteLine(str2.LastIndexOf("Podgorica"));
+
+        int numberOf_i = 0;
+        int startIndex = 0;
+
+        while (str2.IndexOf('i', startIndex) > 0)
+        {
+            startIndex = str2.IndexOf('i', startIndex);
+            Console.WriteLine(startIndex);
+            startIndex++;
+            numberOf_i++;
+
+        }
+
+        Console.WriteLine("Number of 'i' = " + numberOf_i);
+        Console.WriteLine();
+        Console.WriteLine(RemoveAll(str2, "Podgorica"));
+
+        string RemoveAll(string input, string target)
+        {
+            string result = input;
+
+            for (int i = 0; (i = result.ToLower().IndexOf(target.ToLower(), i)) > 0;)
+            {
+                result = result.Remove(i, target.Length);
+            }
+
+            return result;
+        }
+
+
+        //объединяет элементы последова ельности(массива) в строку
+        Console.WriteLine("\n---Join");
+
+        string newStringFromArray = String.Join(' ', splitStr);
+
+        Console.WriteLine(newStringFromArray);
+        Console.WriteLine();
+
+        int[] intArr = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(String.Join(", ", intArr));
+
+        string str3 = String.Join("", str2.Split(',', '.', ':', '(', ')'));
+        Console.WriteLine(str3);
+
+        //дополняют строку пробелами до указанной длины
+        Console.WriteLine("\n--- PadLeft & PadRight");
+        Console.WriteLine(str1.PadRight(10) + '|'));
+        Console.WriteLine(str1.PadLeft(10) + '|'));
+        Console.WriteLine(str1.PadRight(5) + '|'));
+
+        //то же самое с помощью форматирования
+        Console.WriteLine($"{str1,-10}|");
+        Console.WriteLine($"{str1,10}|");
+    }
+}
+
