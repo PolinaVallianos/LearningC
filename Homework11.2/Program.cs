ac08fce87f3efea7017faae0bfbe0a6bfc12a32a
PolinaVallianos <115357960+PolinaVallianos@users.noreply.github.com>
2022-12-27 16:08:17 +0100

    Lesson12

--- Homework11.2/Program.cs
+++ Homework11.2/Program.cs
@@ -1,17 +1,148 @@
-﻿namespace Homework11._2;
+﻿using System.Text;
+
+namespace Homework11._2;
 class Program
 {
     static void Main(string[] args)
     {
-        string input = "What, exactly is Good and Bad? We say things are good and bad but how do we decide this?\n" +
-            "Bad is defined as the opposite of good, so this means something assessed as such is unfit or morally reprehensible.\n" +
-            "when something is moral, we say an act is good or bad. We also may say that the person is good or bad, although this implies that a person who does one bad thing has permanently bad intent and will always do bad things, and vice versa.\n" +
-            "This makes good and bad uniquely human. Animals do not have morals, so predatory and possibly cruel (in our view) acts are neither good nor bad. We cannot say, for example, that a lion which kills a terrified buffalo is bad (or good).\n" +
-            "Deciding whether something is good or bad is an evaluation, a decision. the concept of good and bad is important in our decision-making and we use our notions of goodness to censor both our own choices and those of others.\n" +
-            "good and bad are defined by people. When nobody knows and there is nobody there to judge, then good and bad do not inherently exist.\n" +
-            "Some who wear the goodguy badge are also actually good people.";
+        string str1 = "Abcdefgh";
+        string str2 = "Cities of Montenegro: Podgorica, Bar, Cetine. Podgorica is the capital. (podgorica)";
 
-        input = input.Replace('?', '.');
+        int maxLeength = 0;
+
+        foreach (var item in str2.Split())
+        {
+            Console.WriteLine(item);
+            maxLeength = maxLeength > item.Length ? maxLeength : item.Length;
+        }
+
+        Console.WriteLine();
+
+        //дополняем пробелами слева
+        foreach (var item in str2.Split())
+        {
+            Console.WriteLine(item.PadLeft(maxLeength));
+        }
+
+        //выравнивание по центру
+        foreach (var item in str2.Split())
+        {
+            Console.WriteLine('|' +
+                (item.
+                    PadLeft(maxLeength - (maxLeength - item.Length) / 2)).
+                    PadRight(maxLeength)+
+                '|');
+        }
+
+        //TrimSplit и TrimEnd - обрезают строкуу сначала и в конце
+
+        //преобразует строку в массив символов
+        Console.WriteLine("\n---ToCharArray");
+
+        char[] charArray1, charArray2;
+
+        charArray1 = str1.ToCharArray();
+        charArray2 = str2.ToCharArray(2, 4);
+        Console.WriteLine(String.Join(' ', charArray2));
+
+        //дополнительные конструкторы строк
+        Console.WriteLine("\n---new String: ");
+
+        string str3, str4;
+
+        //создание строки из массива символов
+        str3 = new string(charArray2);
+
+        str4 = new string(charArray1, 2, 4);
+
+        Console.WriteLine(str3 == str4);
+
+        //создает строку из повторяющихся символов
+
+        str4 = new string('-', 20);
+        Console.WriteLine(str4);
+
+        //проверяет является ли строка пустой или не инициализированной
+        Console.WriteLine("\n---IsNullOrEmpty");
+
+        str4 = "";
+
+        if (String.IsNullOrEmpty(str4) == false)
+        {
+            Console.WriteLine("str4 is not empty: " + str4);
+        }
+
+
+        //
+        str4 = " \n\t";
+
+        if (String.IsNullOrWhiteSpace(str4) == false)
+        {
+            Console.WriteLine("str4 is not empty: " + str4);
+        }
+
+        //класс StringBuilder
+        Console.WriteLine("\n---StringBuilder: ");
+
+        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder("Hello, world!");
+
+        str4 = sb.ToString();
+        Console.WriteLine(sb);
+
+        //длина и ёмкость
+        Console.WriteLine("sb.Length = " + sb.Length);
+        Console.WriteLine("sb.Capacity = " + sb.Capacity);
+
+        //добавление строки в конец
+        sb.Append(" Podgorica.");
+
+        Console.WriteLine(sb);
+        Console.WriteLine("sb.Length = " + sb.Length);
+        Console.WriteLine("sb.Capacity = " + sb.Capacity);
+
+        Console.WriteLine();
+
+        //очищает хранимое значение
+        sb.Clear();
+        sb.Append("Podgorica");
+        Console.WriteLine(sb);
+        Console.WriteLine("sb.Length = " + sb.Length);
+        Console.WriteLine("sb.Capacity = " + sb.Capacity);
+
+        sb = new StringBuilder(50);
+        sb.Append("Podgorica");
+        Console.WriteLine(sb);
+        Console.WriteLine("sb.Length = " + sb.Length);
+        Console.WriteLine("sb.Capacity = " + sb.Capacity);
+
+        //чтение и запись файлов
+        Console.WriteLine("\n---Class File: ");
+
+        string filePath = @"input.txt";
+
+        string input = File.ReadAllText(filePath);
+        input = File.ReadAllText(@"input.txt");
+
+        Console.WriteLine(input);
+
+        string[] strings = File.ReadAllLines(filePath);
+        foreach (var item in strings)
+        {
+            Console.Write(item + " ");
+        }
+
+        string outPath = @"Data/output.txt";
+        //запись строки в файл
+        //File.WriteAllText(outPath, input);
+
+        //запись массива строк в файл
+        File.WriteAllLines(outPath, strings);
+
+        File.AppendAllText(outPath, "\n New text\n");
+
+        File.AppendAllLines(outPath, strings);
     }
+
 }
 
--- Homework11/Program.cs
+++ Homework11/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Homework11;class Program{    static void Main(string[] args)    {        string input = "What, exactly is Good and Bad? We say things are good and bad but how do we decide this?\n" +            "Bad is defined as the opposite of good, so this means something assessed as such is unfit or morally reprehensible.\n" +            "when something is moral, we say an act is good or bad. We also may say that the person is good or bad, although this implies that a person who does one bad thing has permanently bad intent and will always do bad things, and vice versa.\n" +            "This makes good and bad uniquely human. Animals do not have morals, so predatory and possibly cruel (in our view) acts are neither good nor bad. We cannot say, for example, that a lion which kills a terrified buffalo is bad (or good).\n" +            "Deciding whether something is good or bad is an evaluation, a decision. the concept of good and bad is important in our decision-making and we use our notions of goodness to censor both our own choices and those of others.\n" +            "good and bad are defined by people. When nobody knows and there is nobody there to judge, then good and bad do not inherently exist.\n" +            "Some who wear the goodguy badge are also actually good people.";        Statistics(input);        Console.WriteLine("Number of bad = " + NumberOfBad(input));        //Console.WriteLine(Censor(input));    }    static void Statistics(string str)    {
+﻿namespace Homework11;class Program{    static void Main(string[] args)    {        string input = "What, exactly is Good and Bad? We say things are good and bad but how do we decide this?\n" +            "Bad is defined as the opposite of good, so this means something assessed as such is unfit or morally reprehensible.\n" +            "when something is moral, we say an act is good or bad. We also may say that the person is good or bad, although this implies that a person who does one bad thing has permanently bad intent and will always do bad things, and vice versa.\n" +            "This makes good and bad uniquely human. Animals do not have morals, so predatory and possibly cruel (in our view) acts are neither good nor bad. We cannot say, for example, that a lion which kills a terrified buffalo is bad (or good).\n" +            "Deciding whether something is good or bad is an evaluation, a decision. the concept of good and bad is important in our decision-making and we use our notions of goodness to censor both our own choices and those of others.\n" +            "good and bad are defined by people. When nobody knows and there is nobody there to judge, then good and bad do not inherently exist.\n" +            "Some who wear the goodguy badge are also actually good people.";        Statistics(input);        Console.WriteLine("Number of bad = " + NumberOfBad(input));        Console.WriteLine(Censor(input));    }    static void Statistics(string str)    {
 
         Console.WriteLine($"Characters = {str.Length}");
         //Буквы
@@ -46,34 +46,53 @@
         }
 
         return numberOf_bad;
-    }    //static string Censor(string str)
-    //{
-    //    string[] newstr = str.Trim('.', ',', '?', '\n').Split(' ');
+    }    static string Censor(string str)
+    {
+        str = str.Replace('?', '.');
+        string[] stringParagraphs = str.Trim(' ', '.').Split('\n');
+        for (int i = 0; i < stringParagraphs.Length; i++)
+        {
+            string fristLetter = stringParagraphs[i].Substring(0, 1);
+            fristLetter = fristLetter.ToUpper();
+            stringParagraphs[i] = fristLetter + stringParagraphs[i].Remove(0,1);
 
-    //    for (int i = 0; i < newstr.Length; i++)
-    //    {
-    //        if (newstr[i] == "bad")
-    //        {
-    //            newstr[i] = "good";
-    //        }
-    //        else
-    //        {
-    //            if (newstr[i] == "good")
-    //            {
-    //                newstr[i] = "bad";
-    //            }
-    //        }
-    //    }
+            string[] stringWords = stringParagraphs[i].Trim(' ').Split(' ');
 
-    //    string newString = "";
-    //    foreach (var item in newstr)
-    //    {
-    //        newString += item + ' ';
-    //        if (item.EndsWith(('\n'))
-    //        {
-    //            item + 1
-    //        }
-    //    }
+            for (int j = 0; j < stringWords.Length; j++)
+            {
+                string word = stringWords[j].Trim(' ', '.', ',', '?', '(', ')');
+                string capitalLetter = word.Substring(0, 1);
+                if (j>0)
+                {
+                    if (stringWords[j-1].EndsWith('.') || stringWords[j-1].EndsWith('.'))
+                    {
+                        stringWords[j] = capitalLetter.ToUpper() + stringWords[j].Remove(0, 1);
+                    }
+                }
 
-    //    return newString;
-    //}}
+                switch (word)
+                {
+                    case "good":
+                        stringWords[j] = stringWords[j].Replace("good", "bad");
+                        break;
+                    case "Good":
+                        stringWords[j] = stringWords[j].Replace("Good", "Bad");
+                        break;
+                    case "bad":
+                        stringWords[j] = stringWords[j].Replace("bad", "good");
+                        break;
+                    case "Bad":
+                        stringWords[j] = stringWords[j].Replace("Bad", "Good");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            stringParagraphs[i] = String.Join(" ", stringWords);
+        }
+
+        string newString = "  " + String.Join("\n  ", stringParagraphs);
+
+        return newString;
+    }}
