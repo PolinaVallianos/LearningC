704dbc6eca9989e4fd199d9a29d1f62c343edc0b
PolinaVallianos <115357960+PolinaVallianos@users.noreply.github.com>
2023-01-22 16:10:53 +0100

    Homework14

---
+++ Homework14/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+
+namespace Homework14;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int minNumber, maxNumber;
+        int[] test = {1, 2, 5, 7};
+        int length = 8;
+        bool result = false;
+        int characters = 0;
+
+        (minNumber, maxNumber) = GetMinMax(75, 8, 12, 12, 3, 100);
+        Console.WriteLine($"Min = {minNumber}, Max = {maxNumber}");
+
+        (minNumber, maxNumber) = GetMinMax(1, 3, 67, 2, 32);
+        Console.WriteLine($"Min = {minNumber}, Max = {maxNumber}");
+
+        (result, characters) = Expand(test, length);
+        Console.WriteLine($"\n{result}, Number of items added: {characters}");
+
+        if (result == true)
+        {
+            int[] newArray = new int[length];
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                newArray[i] = test[i];
+            }
+
+            for (int j = 1, k = 0; k < characters; j++, k++)
+            {
+                newArray[test.Length + k ] = j;
+            }
+
+            for (int i = 0; i < newArray.Length-1; i++)
+            {
+                Console.Write(newArray[i] + ", ");
+            }
+            Console.Write(newArray[newArray.Length-1] + "\n");
+        }
+
+    }
+
+    static (int, int) GetMinMax(params int[] numbers)
+    {
+        int[] result = new int[2];
+        int bigNumber = 0;
+
+        foreach (var item in numbers)
+        {
+            if (item >= bigNumber)
+            {
+                bigNumber = item;
+            }
+        }
+
+        int smallNumber = bigNumber;
+
+        foreach (var item in numbers)
+        {
+            if (item <= smallNumber)
+            {
+                smallNumber = item;
+            }
+        }
+        return (smallNumber, bigNumber);
+    }
+
+    static (bool, int) Expand(int[] array, int length)
+    {
+        bool result = false;
+        int characters = 0;
+
+        if (length > array.Length)
+        {
+            result = true;
+            characters = length - array.Length;
+        }
+        return (result, characters);
+    }
+}
+
--- Lesson14/Program.cs
+++ Lesson14/Program.cs
@@ -34,9 +34,64 @@ class Program
 
         int counter;
 
-        CharCounter("abcdbabababa", 'a', out counter);
+        CharCounter("abcdbabababa", 'a', out counter);//с помощью out можно передать не инициализированную переменную и изменится 
         Console.WriteLine("counteer = " + counter);
 
+        if (CharCounter("abcdbabababa", 'a', out counter))
+        {
+            Console.WriteLine("counteer = " + counter);
+        }
+
+        if (CharCounter("abcdbabababa", 'm', out counter))
+        {
+            Console.WriteLine("counteer = " + counter);
+        }
+
+        Console.WriteLine("\n----- in modifier -----"); //значение не может(!) быть изменено быть изменено внутри метода
+
+        //кортежи - набор сгруппированных значений
+        Console.WriteLine("\n----- Tuples -----"); //значение не может(!) быть изменено быть изменено внутри метода
+
+        (int, int) tuple1 = (2, 4);
+        Console.WriteLine(tuple1);
+
+        //по умолчаниюзначпния в кортеже имеют имена Item1, Item2, Item3..
+        Console.WriteLine("tuple1.Item1 = " + tuple1.Item1);
+
+        //также можно задавать и свои имена
+        (string Name, int Age) person = ("Piter", 22);
+
+        Console.WriteLine($"Name = {person.Name}, Age = {person.Age}");
+
+        var vector2 = (X:0, Y:1);
+
+        vector2.X = 12;
+        vector2.Y = 5;
+
+        vector2 = RandomMove();
+        Console.WriteLine($"X: {vector2.X}; Y: {vector2.Y}");
+
+
+        //объявление кортежа pair с элементами M и N
+        (int M, int N) pair = (12, 13);
+
+        //декомпазиция
+        //объевление перемменных m и n, и присваеваем значения при помощи кортежа pair
+        (int m, int n) = pair;
+
+        //присваевание значений переменным a и b с помощью кортежа
+        (a, b) = RandomMove();
+
+        //обмен значений переменных с помощью кортежей
+        Console.WriteLine($"m = {m}, n = {n}");
+        (m, n) = (n, m);
+        Console.WriteLine($"m = {m}, n = {n}");
+
+        //при сравнении кортежей важны только тип и пордок значений
+        vector2 = (12, 13);
+        Console.WriteLine(pair == vector2);
+        pair.M = 15;
+        Console.WriteLine(pair == vector2);
     }
 
     //передача в метод массива параметров
@@ -75,8 +130,9 @@ class Program
         if (value > max) value = max;
     }
 
-    static void CharCounter(string str, char ch, out int count)
+    static bool CharCounter(string str, char ch, out int count)
     {
+        bool result = false;
         count = 0;
 
         foreach (var item in str)
@@ -86,6 +142,21 @@ class Program
                 count++;
             }
         }
+
+        if (count > 0) result = true;
+
+        return result;
+    }
+
+    //кортеж как возращаемый тип
+    static (int, int) RandomMove()
+    {
+        Random random = new Random();
+        int x, y;
+        x = random.Next(1000);
+        y = random.Next(1000);
+
+        return (x, y);
     }
 }
 
